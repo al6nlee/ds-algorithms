24028a787f04e508417f332f52e7d105f8eb21e2
alan <al6nlee@gmail.com>
2024-10-31 21:48:22 +0800

    double linked list add_by_order

--- 02-linked_list/02-double_linked_list.py
+++ 02-linked_list/02-double_linked_list.py
@@ -21,6 +21,23 @@ class DoubleLinkedList:
         node.next = hero_node
         hero_node.pre = node
 
+    def add_by_order(self, hero_node: DoubleNode):
+        node = self.head
+        while node.next:
+            if node.next.no > hero_node.no:
+                hero_node.next = node.next
+                hero_node.pre = node
+                node.next.pre = hero_node
+                node.next = hero_node
+                return
+            elif node.next.no == hero_node.no:
+                print("添加英雄编号 %d 已经存在，不能添加" % hero_node.no)
+                return
+            else:
+                node = node.next
+        node.next = hero_node
+        hero_node.pre = node
+
     def list(self):
         node = self.head
         if not node.next:
@@ -52,20 +69,31 @@ class DoubleLinkedList:
 
 
 if __name__ == '__main__':
+    # hero_1 = DoubleNode(1, '宋江', '及时雨')
+    # hero_2 = DoubleNode(2, '卢俊义', '玉麒麟')
+    # hero_3 = DoubleNode(3, '吴用', '智多星')
+    # hero_4 = DoubleNode(4, '林冲', '豹子头')
+    # hero_5 = DoubleNode(4, '林冲1', '豹子头1')
+    # hero_list = DoubleLinkedList()
+    # hero_list.add(hero_1)
+    # hero_list.add(hero_2)
+    # hero_list.add(hero_3)
+    # hero_list.add(hero_4)
+    # hero_list.list()
+    # print("update")
+    # hero_list.update(hero_5)
+    # hero_list.list()
+    # print("delete")
+    # hero_list.delete(4)
+    # hero_list.list()
+
     hero_1 = DoubleNode(1, '宋江', '及时雨')
     hero_2 = DoubleNode(2, '卢俊义', '玉麒麟')
     hero_3 = DoubleNode(3, '吴用', '智多星')
     hero_4 = DoubleNode(4, '林冲', '豹子头')
-    hero_5 = DoubleNode(4, '林冲1', '豹子头1')
     hero_list = DoubleLinkedList()
-    hero_list.add(hero_1)
-    hero_list.add(hero_2)
-    hero_list.add(hero_3)
-    hero_list.add(hero_4)
-    hero_list.list()
-    print("update")
-    hero_list.update(hero_5)
-    hero_list.list()
-    print("delete")
-    hero_list.delete(4)
+    hero_list.add_by_order(hero_1)
+    hero_list.add_by_order(hero_3)
+    hero_list.add_by_order(hero_2)
+    hero_list.add_by_order(hero_4)
     hero_list.list()
